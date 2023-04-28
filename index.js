a70003ecb151be1e5a9c103cea86ace4069407cc
unknown <pranshugoyal.cse19@chitkarauniversity.edu.in>
2023-04-28 14:49:33 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const port=8080;
+const router=require('./router');
+const path=require('path');
+app.use('/static',express.static(path.join(__dirname,'public')));
+app.use('/css',express.static(path.join(__dirname,'public/css')));
+
+
+app.use('/router',router);
+
+app.listen(port,()=>{
+    console.log('Server Running');
+})
